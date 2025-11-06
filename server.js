ad0e37f63338faf74edb7eddf87fbfb957fea7bc
electriquemetalman <stivingtatga@gmail.com>
2025-11-06 02:30:17 -0500

    fix beug and build payment getway api

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ import { connectDB } from "./config/db.js"
 import foodRouter from "./routes/foodRoute.js"
 import userRouter from "./routes/userRoute.js"
 import cartRouter from "./routes/cartRoute.js"
+import orderRouter from "./routes/orderRoute.js"
 
 
 //charger le fichier .env
@@ -26,6 +27,7 @@ connectDB()
 app.use("/api/food", foodRouter)
 app.use("/api/user", userRouter)
 app.use("/api/cart", cartRouter)
+app.use("/api/order", orderRouter)
 
 app.get("/", (req, res) => {
   res.status(200).send("Hello from the backend!")
